883eed11d47307dca1785dc6dc6be0088940f6bf
KRahuldeep <168513400+KRahuldeep@users.noreply.github.com>
2026-07-03 16:13:07 +0530

    Add lightbox feature for gallery images
    Implement lightbox functionality for gallery images, allowing images to be displayed in a modal. Add event listeners for closing the lightbox with the escape key and clicking outside the image.

--- script.js
+++ script.js
@@ -183,3 +183,44 @@ document.querySelectorAll("#nav-menu a").forEach(link => {
         navMenu.classList.remove("show");
     });
 });
+document.addEventListener("keydown", (e) => {
+
+    if (e.key === "Escape") {
+        lightbox.style.display = "none";
+    }
+
+});
+
+// ===============================
+// Certificate Lightbox
+// ===============================
+
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.getElementById("close");
+
+galleryImages.forEach(image => {
+
+    image.addEventListener("click", () => {
+
+        lightbox.style.display = "flex";
+        lightboxImg.src = image.src;
+
+    });
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    lightbox.style.display = "none";
+
+});
+
+lightbox.addEventListener("click", (e) => {
+
+    if(e.target === lightbox){
+        lightbox.style.display = "none";
+    }
+
+});
